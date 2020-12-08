5b3fcd527f09da50067702802835765c5cbf05b2
Rick <dirklo@gmail.com>
2020-12-08 19:08:56 +0100

    commit tree c458cb98caf5303523ebd56f8e9dd301e3967be5

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+def prime?(n)
+    if n <= 1
+        return false
+    end
+    marker = n/2.floor
+    while marker > 1
+        if n % marker == 0 
+            return false
+        end
+        marker -= 1
+    end 
+    return true
+end
